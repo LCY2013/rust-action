d2024ef07f67a6c1cfee9950741b5c5fd4d65c53
luocy <luocy@dustess.com>
2022-07-27 16:01:46 +0800

    🎸 feat: use env param info

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,7 +1,12 @@
+// cargo run -- https://www.rust-lang.org rust.md, 命令行信息
 fn main() {
     println!("Hello, world!");
     let name = "fufeng".to_string();
     std::thread::spawn(|| {
         //println!("hello {}", name)
     });
+    // 获取系统环境参数信息
+    for arg in std::env::args() {
+        println!("{}", arg);
+    }
 }
